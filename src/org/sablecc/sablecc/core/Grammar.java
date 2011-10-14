3056b9be19cb6c4be88c685d574687b36c986cc0
Jean Privat <jean@pryen.org>
2011-10-14 10:15:20 -0400

    Refuse when there the lexer and the parser are both empty.
    Signed-off-by: Jean Privat <jean@pryen.org>

--- src/org/sablecc/sablecc/core/Grammar.java
+++ src/org/sablecc/sablecc/core/Grammar.java
@@ -104,6 +104,11 @@ public class Grammar
             }
 
         }
+
+        if (this.globalAnonymousContext == null && this.namedContexts.isEmpty()) {
+            throw SemanticException
+                    .genericError("The Lexer and the Parser are both empty.");
+        }
     }
 
     @Override
